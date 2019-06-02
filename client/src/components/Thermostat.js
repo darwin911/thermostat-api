6113fcfa2228200d144c7a2603ea04065a9e8b72
Darwin Smith <darwinpsmith@gmail.com>
2019-06-02 15:24:32 -0400

    display data only renders when thermostat.isOn

--- client/src/components/Thermostat.js
+++ client/src/components/Thermostat.js
@@ -6,12 +6,23 @@ class Thermostat extends React.Component {
     return (
       <section className="thermostat">
         <article className="display">
-          <p className="status">
-            {thermostat.isCooling ? "Cooling" : ""}
-            {thermostat.isHeating ? "Heating" : ""}
-            {thermostat.isIdle ? "Idle" : ""}
-          </p>
-          <p className="temp">{thermostat.temp}</p>
+          {thermostat.isOn && (
+            <>
+              <p
+                className="status"
+                style={
+                  thermostat.isCooling
+                    ? { color: "#00ccff" }
+                    : { color: "orangered" }
+                }
+              >
+                {thermostat.isCooling ? "Cooling" : ""}
+                {thermostat.isHeating ? "Heating" : ""}
+                {thermostat.isIdle ? "Idle" : ""}
+              </p>
+              <p className="temp">{thermostat.temp}</p>
+            </>
+          )}
         </article>
       </section>
     );
