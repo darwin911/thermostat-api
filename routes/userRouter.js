c80679e9c3f1ae1195bc41253ce2e214fd0a677e
Darwin Smith <darwinpsmith@gmail.com>
2019-06-02 10:04:46 -0400

    thermostat route updates settings (temp, heating, cooling)

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -61,6 +61,12 @@ userRouter.post("/:user_id/thermostat", async (req, res) => {
   console.log("thermostat api");
   const user = await User.findOne({ where: { id: req.body.userId } });
   const thermostat = await user.getThermostat();
+  await thermostat.update({ 
+    temperature: req.body.temp,
+    heating: req.body.isHeating,
+    cooling: req.body.isCooling,
+    on: req.body.isOn,
+   });
   const thermostatData = {
     isCooling: thermostat.cooling,
     isHeating: thermostat.heating,
@@ -68,8 +74,8 @@ userRouter.post("/:user_id/thermostat", async (req, res) => {
     isOn: thermostat.on,
     temp: thermostat.temperature,
     id: thermostat.id,
-    userId: thermostat.userId,
-  }
+    userId: thermostat.userId
+  };
   res.json(thermostatData);
 });
 
