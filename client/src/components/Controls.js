95d46989aaaa98aa4e3c7f36b7e29f44a4f2cd90
Darwin Smith <darwinpsmith@gmail.com>
2019-06-02 10:05:59 -0400

    temp controls for Controls Comp connected to api

--- client/src/components/Controls.js
+++ client/src/components/Controls.js
@@ -1,26 +1,51 @@
 import React from "react";
 import { Icon } from "semantic-ui-react";
 
-const Controls = ({ thermostat, toggleCooling, toggleHeating, toggleOn }) => {
+const Controls = ({
+  thermostat,
+  toggleCooling,
+  toggleHeating,
+  toggleOn,
+  lowerTemp,
+  increaseTemp
+}) => {
   return (
     <aside className="controls">
-      <button className={thermostat.isOn ? "powerOn" : ""} onClick={toggleOn}>
+      <button
+        className={(thermostat.isOn ? "powerOn" : "") + " control-btn"}
+        onClick={toggleOn}
+      >
         <Icon name="power off" />
       </button>
       <button
-        className={thermostat.isHeating ? "heating" : ""}
+        className={(thermostat.isHeating ? "heating" : "") + " control-btn"}
         onClick={toggleHeating}
         disabled={!thermostat.isOn}
       >
         <Icon name="fire" />
       </button>
       <button
-        className={thermostat.isCooling ? "cooling" : ""}
+        className={(thermostat.isCooling ? "cooling" : "") + " control-btn"}
         onClick={toggleCooling}
         disabled={!thermostat.isOn}
       >
         <Icon name="snowflake" />
       </button>
+      <button
+        className={(thermostat.isOn ? "minus-temp" : "") + " control-btn"}
+        onClick={lowerTemp}
+        disabled={!thermostat.isOn}
+      >
+        <Icon name="minus" />
+      </button>
+      <button
+        className={(thermostat.isOn ? "add-temp" : "") + " control-btn"}
+        onClick={increaseTemp}
+        disabled={!thermostat.isOn}
+      >
+        <Icon name="plus" />
+      </button>
+
       <p className="">{thermostat.isIdle ? "idle" : "---"}</p>
     </aside>
   );
