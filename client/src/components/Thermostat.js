91e95d716a8d13431a2e4f8523968aca47c61d66
Darwin Smith <darwinpsmith@gmail.com>
2019-06-02 10:05:10 -0400

    refactor: Controls removes from Thermostat comp

--- client/src/components/Thermostat.js
+++ client/src/components/Thermostat.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Controls from "./Controls";
 
 class Thermostat extends React.Component {
   render() {
-    const { thermostat, toggleCooling, toggleHeating, toggleOn } = this.props;
+    const { thermostat } = this.props;
     return (
       <section className="thermostat">
         <article className="display">
@@ -14,12 +13,6 @@ class Thermostat extends React.Component {
           </p>
           <p className="temp">{thermostat.temp}</p>
         </article>
-        <Controls
-          thermostat={thermostat}
-          toggleCooling={toggleCooling}
-          toggleHeating={toggleHeating}
-          toggleOn={toggleOn}
-        />
       </section>
     );
   }
